2e53aff15dbb34a69e239e675e04842e0cd4b318
An10nella <135998311+An10nella@users.noreply.github.com>
2023-08-01 20:48:20 +0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-let username =['admin01', 'admin02', 'admin03']
+
+
+const addBtnEl = document.getElementById('addBtn')
+const userListEl = document.getElementById('userList')
+const liEl = document.createElement('li')
+const addToListEl = document.getElementById('addToList')
+document.title ='list'
+
+let username =[]
 // console.log(username )
 
 // username.push('admin04')
@@ -14,3 +22,16 @@ let username =['admin01', 'admin02', 'admin03']
 // console.log(username)
 // username.unshift('admin00') 
 
+addBtnEl.addEventListener('click', function(){
+    username = []
+    let newInput = addToListEl.value
+    username.push(newInput)
+
+    for(let i in username){
+        userListEl.innerHTML += `<li style="margin-left:20px">${username[i]} </li>`
+
+    }
+    addToListEl.value=''
+    //addToList.appendChild() --> add list wara ba3d
+    
+})
