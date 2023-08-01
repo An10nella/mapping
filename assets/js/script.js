2776dfd1b3a3d687df621faa2539884ce2e723f5
An10nella <135998311+An10nella@users.noreply.github.com>
2023-08-01 19:46:40 +0300

    add button & text

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let username =['admin01', 'admin02', 'admin03']
+console.log(username )
+
+username.push('admin04')
+username.push('admin05')
+console.log(username)
+
+username.pop()
+username.pop() //erase last one
+console.log(username)
+
+// username.shift()//erase first one
+username.unshift('admin00')
+console.log(username)
+
